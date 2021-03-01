d2d4cf338a0b611067727fbda3a751e32407f853
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-02-28 21:21:52 -0600

    Update Nav.js and add styling

--- protfolio/src/components/Nav.js
+++ protfolio/src/components/Nav.js
@@ -2,7 +2,42 @@ import React from "react";
 import Style from "./Style.css";
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <div className="header">
+      <nav className="navbar navbar-expand-lg">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="fa fa-bars"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/projects.html">
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact.html">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Nav;
