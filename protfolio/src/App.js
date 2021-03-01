c57d6264a73862be034ed583c60f960bea7ef348
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-02-28 21:13:21 -0600

    Add components and link bootstrap

--- protfolio/src/App.js
+++ protfolio/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>HELLO HELLO HELLO</h1>
     </div>
   );
---
+++ protfolio/src/components/Nav.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Style from "./Style.css";
+
+const Nav = () => {
+  return <div></div>;
+};
+
+export default Nav;
